cd813b922223d7a79b006bc17ca374a585df3ebf
Aaron Francis <aarondfrancis@gmail.com>
2021-07-31 14:37:18 -0500

    First

---
+++ README.md
@@ -0,0 +1,89 @@
+# Torchlight Client for Ibis
+
+
+🚨 This package is currently waiting on [a PR to be merged](https://github.com/themsaid/ibis/pull/40) for Ibis. 🚨
+
+It won't work until then.
+
+----
+
+A [Torchlight](https://torchlight.dev) syntax highlighting extension for the ebook builder [Ibis](https://github.com/themsaid/ibis).
+
+Torchlight is a VS Code-compatible syntax highlighter that requires no JavaScript, supports every language, every VS Code theme, line highlighting, git diffing, and more.
+
+## Installation
+
+To install, require the package from composer:
+
+```
+composer require torchlight/torchlight-ibis
+```
+
+If you haven't already, Composer will ask you to create a `composer.json` file in the root of your book directory.  
+
+In your `ibis.php` file, add the following to your configuration:
+
+```php
+'configure_commonmark' => function ($environment) {
+    TorchlightExtension::make()->register($environment);
+},
+```
+
+At the top of your `ibis.php` file, you'll also need to add the composer autoloader and import the Torchlight namespace:
+
+```php
+<?php
+
+use Torchlight\Ibis\TorchlightExtension;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+return [
+    /**
+     * The book title.
+     */
+    'title' => 'Laravel Queues in Action',
+
+    // .....
+];
+```
+
+To publish the Torchlight configuration file, you may run `./vendor/bin/torchlight`. It will place a `torchlight.php` file next to your `ibis.php` file.
+
+You'll need to add a few styles to your stylesheet. These are slightly different from the default styles that Torchlight uses, because Ibis uses mPDF to convert the HTML to a PDF, and it has a few specific requirements.
+
+Here is a good starting set of styles, but feel free to tweak them to match your needs.
+
+```css
+/*
+ Margin and rounding are personal preferences,
+ overflow-x-auto is recommended.
+*/
+pre {
+    page-break-inside: avoid;
+    border: solid 1px #eee;
+    margin-bottom: 30px;
+    border-radius: 5px;
+    overflow-x: auto;
+}
+
+/*
+ Add some vertical padding and expand the width
+ to fill its container. The horizontal padding
+ comes at the line level so that background
+ colors extend edge to edge.
+*/
+pre div.torchlight {
+    padding-top: 16px;
+    padding-bottom: 16px;
+}
+
+/*
+ Horizontal line padding to match the vertical
+ padding from the code block above. Vertical 
+ for line height.
+*/
+pre div.torchlight .line {
+    padding: 2px 16px;
+}
+```
---
+++ src/TorchlightExtension.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author Aaron Francis <aarondfrancis@gmail.com|https://twitter.com/aarondfrancis>
+ */
+
+namespace Torchlight\Ibis;
+
+use Illuminate\Cache\FileStore;
+use Illuminate\Cache\Repository;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Http\Client\Factory;
+use Illuminate\Support\Arr;
+use League\CommonMark\ConfigurableEnvironmentInterface;
+use Torchlight\Block;
+use Torchlight\Manager;
+use Torchlight\Torchlight;
+use Illuminate\Support\Facades\Http;
+use Torchlight\Commonmark\TorchlightExtension as TorchlightCommonmark;
+
+class TorchlightExtension
+{
+    protected $baseDir;
+
+    public static function make($baseDir = null)
+    {
+        return new static($baseDir);
+    }
+
+    public function __construct($baseDir = null)
+    {
+        $this->baseDir = $baseDir ?? getcwd();
+    }
+
+    public function register(ConfigurableEnvironmentInterface $environment)
+    {
+        $this->swapFacadeInstances();
+        $this->registerConfig();
+        $this->registerCache();
+
+        // Throw exceptions when something goes wrong, since this
+        // is a local build tool.
+        Torchlight::overrideEnvironment('local');
+
+        (new TorchlightCommonmark)
+            // Add a custom renderer for Ibis, because mPDF doesn't allow you
+            // to change a `code` tag to display:block, which we absolutely
+            // need. https://mpdf.github.io/html-support/html-tags.html.
+            ->useCustomBlockRenderer(function (Block $block) {
+                return "<pre><div class='{$block->classes}' style='{$block->styles}'>{$block->highlighted}</div></pre>";
+            })
+            // Bind the whole thing into the Commonmark environment.
+            ->register($environment);
+    }
+
+    protected function swapFacadeInstances()
+    {
+        // There is no container in an Ibis project,
+        // so we just swap instances in.
+        Torchlight::swap(new Manager);
+        Http::swap(new Factory);
+    }
+
+    protected function registerConfig()
+    {
+        // Pull the configuration from a file named 'torchlight.php' in the base dir.
+        $file = $this->baseDir . DIRECTORY_SEPARATOR . 'torchlight.php';
+
+        $config = file_exists($file) ? require $file : [];
+
+        Torchlight::getConfigUsing(function ($key, $default) use ($config) {
+            return Arr::get($config, $key, $default);
+        });
+    }
+
+    protected function registerCache()
+    {
+        // Build a Laravel-standard Cache store so we don't
+        // have to send API requests every single time.
+        Torchlight::setCacheInstance(new Repository(
+            new FileStore(new Filesystem, $this->baseDir . DIRECTORY_SEPARATOR . 'cache')
+        ));
+    }
+
+}
